f1a99b138e100c3ac5688d49aae647f580730d23
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-21 17:11:31 +0300

    Merge the age in the constructor with the other number values

--- accounts/users.go
+++ accounts/users.go
@@ -16,7 +16,7 @@ type Names struct {
 func New(firstName, lastName string, age, accNumber int, balance float64) *User {
 	user := &User{}
 	user.Name.FirstName, user.Name.LastName = firstName, lastName
-	user.Age = age
-	user.Acc.Balance, user.Acc.Number = balance, accNumber
+	user.Acc.Balance, user.Age, user.Acc.Number = balance, age, accNumber
+
 	return user
 }
