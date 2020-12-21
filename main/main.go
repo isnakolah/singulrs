c93c237b02d148b0eea5af1c1b83fdbaf070c967
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-21 14:53:04 +0300

    create a user init func

--- main/main.go
+++ main/main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+// Accounts struct shows the blueprint of Accounts
+type Accounts struct {
+	Number  int
+	Balance int
+}
+
+// User is the blueprint of the user
+type User struct {
+	Name Names
+	Age  int
+	Acc  Accounts
+}
+
+// Names shows the names of the user
+type Names struct {
+	FirstName, MiddleName, LastName string
+}
+
+func userInit(firstName, middleName, lastName string, age, balance, accNumber int) *User {
+	user := new(User)
+	user.Name.FirstName, user.Name.MiddleName, user.Name.LastName = firstName, middleName, lastName
+	user.Age = 20
+	user.Acc.Balance, user.Acc.Number = 1000, 1
+	return user
+}
+
 func main() {
-	fmt.Println("works")
+	david := userInit("David", "Aniel", "Beast", 33, 3000, 23)
+
+	fmt.Println(david.Acc)
 }
