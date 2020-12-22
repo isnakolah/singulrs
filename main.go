de7c4d1f542856c2d531c76e4e844d3d076e8d86
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-22 13:09:24 +0300

    Add brand to the transaction

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"bankGolang/transactions"
+	"fmt"
+)
+
 func main() {
+	shoe := transactions.NewItem("Shoe", map[string]float64{"Balenciagas": 3000.02}, "pair(s)")
+	trans1 := transactions.NewTransaction(shoe, map[string]float64{"North Star": 300}, 3)
+
+	fmt.Println(trans1.ItemBrand)
 }
--- transactions/transactions.go
+++ transactions/transactions.go
@@ -2,15 +2,21 @@ package transactions
 
 // Transaction struct defines the blueprint of the transaction
 type Transaction struct {
-	Item   Item
-	Amount uint
+	Item      *Item
+	ItemBrand Brand
+	Amount    uint
 }
 
 // NewTransaction function creates new transaction
-func NewTransaction(item Item, amount uint) (transaction *Transaction) {
+func NewTransaction(item *Item, itemBrand map[string]float64, amount uint) (transaction *Transaction) {
 	transaction = &Transaction{}
 
 	transaction.Item = item
 	transaction.Amount = amount
+
+	for brand, price := range itemBrand {
+		transaction.ItemBrand = Brand{brand, price}
+	}
+
 	return
 }
