0eb65c88d5e55b3e6d060dc82c2775604c9d6718
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 03:19:44 +0300

    Add error messages in transactions_test

--- transactions/transactions_test.go
+++ transactions/transactions_test.go
@@ -6,10 +6,20 @@ func TestNewItem(t *testing.T) {
 	sugar := NewItem("Sugar", map[string]float64{"Kabras": 110, "Mumias": 130}, "kg(s)")
 
 	if sugar.Name != "Sugar" {
-		t.Errorf("New item not captured")
+		t.Error(
+			"\n-> Valid item not captured",
+			"\n-> For ItemName",
+			"expected", "Sugar",
+			"got", sugar.Name,
+		)
 	}
 
-	if len(sugar.Brands) != 2 {
-		t.Errorf("Brands not added")
+	if len(sugar.Brands) == 2 {
+		t.Error(
+			"\n-> Brands of item not added.",
+			"\n-> For len(Brands)",
+			"expected", 2,
+			"got", len(sugar.Brands),
+		)
 	}
 }
