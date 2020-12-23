3edf68833f78d9b44cb7dd7c5e82081556a0d227
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 03:57:13 +0300

    Create an ErrorMessage function to handle formating of the error messages

--- accounts/accounts_test.go
+++ accounts/accounts_test.go
@@ -1,45 +1,40 @@
 package accounts
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ErrorMessage(baseMessage, label string, expected, got float64) string {
+	return fmt.Sprintf("\n\n-> %s. \n-> For %q expected %g got %g\n\n", baseMessage, label, expected, got)
+}
 
 func TestNewUser(t *testing.T) {
 	// Test for a valid input
 	daniel, err := NewUser("Daniel", "Nakolah", 20, -1000)
 
-	if err != nil {
-		t.Error(
-			"\n\n-> Vaid deposit not successful",
-			"For", "Account Balance",
-			"expected", 1000,
-			"got", daniel.Acc.Balance,
-			"\n-> Error:", err, "\n",
-		)
+	if err == nil {
+		t.Errorf(ErrorMessage(
+			"Valid deposit not successful", "Account Balance", float64(1000), daniel.Acc.Balance,
+		))
 	}
 
 	// Test for an invalid account balance
 	jane, err := NewUser("Jane", "Doe", 18, -1234)
 
-	if err == nil && jane.Acc.Balance != 0 {
-		t.Error(
-			"\n-> Invalid deposit, error not raised",
-			"\n-> For", jane.Acc.Balance,
-			"expected", 0,
-			"got", jane.Acc.Balance,
-			"\n-> Error:", err,
-		)
+	if err == nil {
+		t.Errorf(ErrorMessage(
+			"Invalid deposit, error not raised", "Account Balance", float64(0), jane.Acc.Balance,
+		))
 	}
 
 	// Test for an invalid age
 	john, err := NewUser("John", "Doe", 17, 1000)
 
-	if err == nil && john.Name.FirstName != "" {
-		t.Error(
-			"-> Invalid age, error not raised",
-			"\n-> For", john.Age,
-			"expected", 0,
-			"got", john.Age,
-			"\n-> Error:", err,
-		)
+	if err == nil {
+		t.Errorf(ErrorMessage(
+			"Invalid age, error not raised", "Age", float64(0), float64(john.Age),
+		))
 	}
 
 }
@@ -50,14 +45,10 @@ func TestDeposit(t *testing.T) {
 
 	jane.Deposit(1000)
 
-	if jane.Acc.Balance != 2000 {
-		t.Error(
-			"\n-> Valid deposit not successful",
-			"\n-> For", jane.Name.FirstName, "account",
-			"expected", 2000,
-			"got", jane.Acc.Balance,
-			"\n-> Error:", err,
-		)
+	if jane.Acc.Balance != 2000 || err != nil {
+		t.Errorf(ErrorMessage(
+			"Valid deposit not successful", "Account Balance", float64(2000), jane.Acc.Balance,
+		))
 	}
 }
 
@@ -67,12 +58,9 @@ func TestWithdraw(t *testing.T) {
 	john.Withdraw(500)
 
 	if john.Acc.Balance != 500 {
-		t.Error(
-			"\n-> Valid withdrawal not successful",
-			"\n-> For", john.Name.FirstName, "account",
-			"expected", 500,
-			"got", john.Acc.Balance,
-		)
+		t.Errorf(ErrorMessage(
+			"Valid withdrawal not successful", "Account Balance", float64(500), john.Acc.Balance,
+		))
 	}
 
 	// Test for invalid withdrawal
@@ -80,14 +68,10 @@ func TestWithdraw(t *testing.T) {
 
 	jane.Withdraw(2000)
 
-	if jane.Acc.Balance != 1000 && err == nil {
-		t.Error(
-			"\n-> Invalid withdrawal, error not raised",
-			"\n-> For", jane.Name.FirstName, "account",
-			"expected", 1000,
-			"got", john.Acc.Balance,
-			"\n-> Error:", err,
-		)
+	if jane.Acc.Balance != 1000 || err == nil {
+		t.Errorf(ErrorMessage(
+			"Invalid withdrawal, error not raised", "Account Balance", float64(1000), jane.Acc.Balance,
+		))
 	}
 
 }
