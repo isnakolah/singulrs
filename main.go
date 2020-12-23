7900c52cd5f72967ce1fdbd6999def69c9119a16
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 07:09:46 +0300

    Return nill if transaction amount < 1

--- main.go
+++ main.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	sugar := transactions.NewItem("Sugar", map[string]float64{"Kabras": 110, "Mumias": 130}, "kg(s)")
+	transaction1 := transactions.NewTransaction(sugar, map[string]float64{"Kabras": 110}, 1)
+	fmt.Println(transaction1)
 	fmt.Printf("%T", sugar.Brands)
 }
--- transactions/transactions.go
+++ transactions/transactions.go
@@ -13,27 +13,28 @@ type Transaction struct {
 // If the brand is not in the list of item brands
 // The function adds it to the item brands slice
 func NewTransaction(item *Item, itemBrand map[string]float64, amount uint) (transaction *Transaction) {
-	transaction = &Transaction{}
-	transaction.Item = item
-	transaction.Amount = amount
-	transaction.ItemBrand = itemBrand
+	if amount > 0 {
+		transaction = &Transaction{}
+		transaction.Item = item
+		transaction.Amount = amount
+		transaction.ItemBrand = itemBrand
 
-	var brandName string
-	for name := range itemBrand {
-		brandName = name
-	}
-	findBrand := func() (available bool) {
-		available = false
-		for _, item := range transaction.Item.Brands {
-			if _, ok := item[brandName]; ok {
-				available = true
-			}
+		var brandName string
+		for name := range itemBrand {
+			brandName = name
 		}
-		return
+		findBrand := func() (available bool) {
+			available = false
+			for _, item := range transaction.Item.Brands {
+				if _, ok := item[brandName]; ok {
+					available = true
+				}
+			}
+			return
+		}
+		if !findBrand() {
+			transaction.Item.AddBrand(itemBrand)
+		}
 	}
-	if !findBrand() {
-		transaction.Item.AddBrand(itemBrand)
-	}
-
 	return
 }
