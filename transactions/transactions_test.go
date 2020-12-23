5a493270d56316d7b50b8d076376531222478329
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 04:57:46 +0300

    Update the tests

--- transactions/transactions_test.go
+++ transactions/transactions_test.go
@@ -1,25 +1,24 @@
 package transactions
 
-import "testing"
+import (
+	"bankGolang/utils"
+	"testing"
+)
 
 func TestNewItem(t *testing.T) {
 	sugar := NewItem("Sugar", map[string]float64{"Kabras": 110, "Mumias": 130}, "kg(s)")
 
-	if sugar.Name != "Sugar" {
-		t.Error(
-			"\n-> Valid item not captured",
-			"\n-> For ItemName",
-			"expected", "Sugar",
-			"got", sugar.Name,
-		)
+	for _, price := range sugar.Brands[0] {
+		if price != 110 {
+			t.Errorf(utils.ErrorMessage(
+				"Valid item not captured", "Brand Price", float64(110), price,
+			))
+		}
 	}
 
 	if len(sugar.Brands) != 2 {
-		t.Error(
-			"\n-> Brands of item not added.",
-			"\n-> For len(Brands)",
-			"expected", 2,
-			"got", len(sugar.Brands),
-		)
+		t.Errorf(utils.ErrorMessage(
+			"Brands of item not added.", "len(Brands)", float64(2), float64(len(sugar.Brands)),
+		))
 	}
 }
