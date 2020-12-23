56680105f9b0ddce634daa35527022251a81c51a
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 06:07:33 +0300

    Update main method

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-	shoe := transactions.NewItem("Shoe", map[string]float64{"Balenciagas": 3000.02}, "pair(s)")
-	fmt.Println(shoe.Brands)
-	trans1 := transactions.NewTransaction(shoe, map[string]float64{"North Star": 300}, 3)
-	trans2 := transactions.NewTransaction(shoe, map[string]float64{"North Star": 300}, 1)
-	trans3 := transactions.NewTransaction(shoe, map[string]float64{"North Star": 300}, 1)
-
-	fmt.Println(shoe.Brands)
-	fmt.Println(trans1.ItemBrand, trans2.ItemBrand, trans3.ItemBrand)
-
+	sugar := transactions.NewItem("Sugar", map[string]float64{"Kabras": 110, "Mumias": 130}, "kg(s)")
+	fmt.Printf("%T", sugar.Brands)
 }
