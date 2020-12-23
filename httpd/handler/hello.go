5d0444be362000b4d8b6401a6cb83b4fdae01edb
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 11:14:32 +0300

    Remove the hello.go handler

--- httpd/handler/hello.go
+++ httpd/handler/hello.go
@@ -1,31 +1,10 @@
 package handler
 
 import (
-	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-// Hello func displays hello world
-func Hello(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set(
-		"Content-Type",
-		"text/html",
-	)
-	io.WriteString(
-		res,
-		`<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Hello World</title>
-            </head>
-            <body>
-                Hello, World!
-            </body>
-        </html>`,
-	)
-}
 
 // PingGet returns the statuscode of OK and JSON response of a map
 func PingGet() gin.HandlerFunc {
