62d8798a0024856138f3734e123c4901ef6cfd47
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-23 08:22:15 +0300

    Create checks for new item

--- transactions/item.go
+++ transactions/item.go
@@ -1,10 +1,22 @@
 package transactions
 
+import (
+	"errors"
+	"strings"
+)
+
+// #TODO remove the item list of brands, to be just a map
+
 // Item struct for the item type
 type Item struct {
 	Name    string
 	Brands  []map[string]float64
 	Measure string
+}
+
+// Message struct for the messages
+type Message struct {
+	Value string
 }
 
 // #TODO RemoveBrand()
@@ -16,12 +28,21 @@ func (item *Item) AddBrand(brands map[string]float64) {
 	}
 }
 
-// NewItem function creates a new item
-func NewItem(name string, brands map[string]float64, unit string) *Item {
-	item := &Item{}
-	item.Name = name
-	item.Measure = unit
-	item.AddBrand(brands)
+// #TODO Get the unit of a good from the web
 
-	return item
+// NewItem function creates a new item
+func NewItem(name string, brands map[string]float64, unit string) (item *Item, err error) {
+	if strings.TrimSpace(name) != "" {
+		item = &Item{}
+		item.Name = name
+		item.AddBrand(brands)
+		if strings.TrimSpace(unit) != "" {
+			item.Measure = unit
+			return
+		}
+		err = errors.New("invalid unit, empty string")
+		return
+	}
+	err = errors.New("invalid name, empty string")
+	return
 }
