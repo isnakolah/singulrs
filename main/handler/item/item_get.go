27b0b742f43c7757083340f3aa477ac08463763b
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-24 15:07:31 +0300

    Move item_get.go into package item handler

---
+++ main/handler/item/item_get.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"bankGolang/transactions"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ItemData struct shapes how the data will look like
+type ItemData struct {
+	// Item map[string]map[string]float64 `json:"item"`
+	Item *transactions.Item `json:"item"`
+}
+
+// ItemGetResponse struct to define how the GET response will look like
+type ItemGetResponse struct {
+	Data    ItemData `json:"data"`
+	Message string   `json:"message"`
+	Error   string   `json:"error"`
+}
+
+// ItemGet returns an item
+func ItemGet() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sugar, err := transactions.NewItem("Sugar", map[string]float64{"Kabras": 110, "Mumias": 110}, "")
+
+		c.JSON(
+			http.StatusOK,
+			ItemGetResponse{ItemData{sugar}, "", err.Error()},
+		)
+	}
+}
