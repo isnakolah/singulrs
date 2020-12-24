619857f332a7aff8c0ae766b381d15d4d3c21600
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-24 18:04:08 +0300

    Test CheckString function

--- src/utils/checkstring/checkstring_test.go
+++ src/utils/checkstring/checkstring_test.go
@@ -1,1 +1,23 @@
 package checkstring
+
+import (
+	"singulr/src/utils/errmessages"
+	"testing"
+)
+
+var tests = map[string]bool{
+	"     ":          false,
+	"Good":           true,
+	"12354262      ": true,
+	"   we 13446   ": true,
+}
+
+func TestValidString(t *testing.T) {
+	for inputedString, expected := range tests {
+		if CheckString(inputedString) != expected {
+			t.Errorf(errmessages.SimpleErrorMessage(
+				"CheckString function not working as expected.",
+			))
+		}
+	}
+}
