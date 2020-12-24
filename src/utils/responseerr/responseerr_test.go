dc30e4a356ec6ab04ed13e17cf581de3352d5b06
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-24 18:41:26 +0300

    Test for GetStrErr

--- src/utils/responseerr/responseerr_test.go
+++ src/utils/responseerr/responseerr_test.go
@@ -1,1 +1,23 @@
 package responseerr
+
+import (
+	"errors"
+	"singulr/src/utils/errmessages"
+	"testing"
+)
+
+func TestGetStrErr(t *testing.T) {
+
+	var err = errors.New("A good error")
+
+	if GetStrErr(err) != "A good error" {
+		t.Errorf(errmessages.SimpleErrorMessage(
+			"GetStrErr not working as expected",
+		))
+	}
+	if GetStrErr(nil) != "" {
+		t.Errorf(errmessages.SimpleErrorMessage(
+			"Nil error not returned as an empty string",
+		))
+	}
+}
