18a20e5c9f15b7a2542f12881655a5df112c4a7e
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-25 01:30:33 +0300

    Create a New function and cart.Add method

--- src/shop/cart/cart.go
+++ src/shop/cart/cart.go
@@ -2,6 +2,23 @@ package cart
 
 import "singulr/src/shop/transaction"
 
-type cart struct {
-	Items []transaction.Transaction `json:"items"`
+// Cart struct is a blueprint for the cart
+type Cart struct {
+	Items []*transaction.Transaction `json:"items"`
+}
+
+// Add method adds items to Items list
+func (cart *Cart) Add(items []*transaction.Transaction) {
+	if items == nil {
+		cart.Items = append(cart.Items, &transaction.Transaction{})
+	}
+	for _, item := range items {
+		cart.Items = append(cart.Items, item)
+	}
+}
+
+// New creates a new transaction
+func New(items ...*transaction.Transaction) (cart Cart) {
+	cart.Add(items)
+	return
 }
