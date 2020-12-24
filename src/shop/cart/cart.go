1e0f21134830d87eebe06a8b07889c27a40cfa27
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-25 01:40:56 +0300

    Add a nil statement is items are nil and return err

--- src/shop/cart/cart.go
+++ src/shop/cart/cart.go
@@ -1,6 +1,9 @@
 package cart
 
-import "singulr/src/shop/transaction"
+import (
+	"errors"
+	"singulr/src/shop/transaction"
+)
 
 // Cart struct is a blueprint for the cart
 type Cart struct {
@@ -8,13 +11,16 @@ type Cart struct {
 }
 
 // Add method adds items to Items list
-func (cart *Cart) Add(items []*transaction.Transaction) {
+func (cart *Cart) Add(items []*transaction.Transaction) (err error) {
 	if items == nil {
-		cart.Items = append(cart.Items, &transaction.Transaction{})
+		cart.Items = nil
+		err = errors.New("no new items entered")
+		return
 	}
 	for _, item := range items {
 		cart.Items = append(cart.Items, item)
 	}
+	return
 }
 
 // New creates a new transaction
