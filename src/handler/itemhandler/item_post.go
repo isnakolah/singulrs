02dcd10b4b76c7756ae797b64bb513fe6192d174
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-12-28 23:15:56 +0300

    Create the item

--- src/handler/itemhandler/item_post.go
+++ src/handler/itemhandler/item_post.go
@@ -1,21 +1,25 @@
 package itemhandler
 
 import (
+	"net/http"
 	"singulr/src/shop/item"
+	"singulr/src/utils/responseerr"
 
 	"github.com/gin-gonic/gin"
 )
 
-// type itemPostRequest struct {
-// 	Name    string
-// 	Brands  map[string]int
-// 	Unit string
-// }
+func createItem(postedItem *item.Item) (createdItem *item.Item, err error) {
+	createdItem, err = item.New(postedItem.Name, postedItem.Brands, postedItem.Unit)
+	return
+}
 
 // PostItem function handles posting of an item
 func PostItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestBody := item.Item{}
 		c.Bind(&requestBody)
+		// c.Status(http.StatusNoContent)
+		_, err := createItem(&requestBody)
+		c.JSON(http.StatusCreated, map[string]string{"error": responseerr.GetStrErr(err)})
 	}
 }
